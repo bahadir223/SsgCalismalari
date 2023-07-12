02359dd77054be1b016a80df300bf02fa0eb7d9a
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-07-13 00:07:33 +0300

    css_Selector class'inda calisma yapildi.

--- src/test/java/css_Selector.java
+++ src/test/java/css_Selector.java
@@ -7,7 +7,6 @@ public class css_Selector extends TestBase {
     @Test
     public void cssSelectorTesti(){
         driver.get("https://theautomationzone.blogspot.com/2020/07/css-selector-practice.html");
-
         System.out.println(driver.findElement(By.cssSelector("#id1")).getText());
         System.out.println(driver.findElement(By.cssSelector(".class1")).getText());
         System.out.println(driver.findElement(By.cssSelector("[name=name1]")).getText());
